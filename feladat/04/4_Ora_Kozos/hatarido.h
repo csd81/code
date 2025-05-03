794f0c79434cc0d08e7ee8459b3400b6dc06ba7e
daniel cs <csorfolydaniel@gmail.com>
2025-05-03 10:52:23 +0200

    +

--- feladat/04/4_Ora_Kozos/hatarido.h
+++ feladat/04/4_Ora_Kozos/hatarido.h
@@ -2,11 +2,39 @@
 #define HATARIDO_H
 
 #include "datum.h"
+#include <string>
 
-class Hatarido : public Datum
+class Hatarido
 {
+    std::string nev;
+    Datum* alpha;
+    Datum* beta;
+    Datum* vegso;
+    static Datum aktualisDatum;
+
 public:
-    Hatarido();
+    Hatarido(std::string n);
+    ~Hatarido();
+
+    Hatarido(const Hatarido& other);
+
+
+
+    std::string getNev() const;
+    void setNev(std::string n);
+    Datum* getAlphaDatum() const;
+    void setAlphaDatum(Datum* al);
+    Datum* getBetaDatum() const;
+    void setBetaDatum(Datum* b);
+    Datum* getVegsoDatum() const;
+    void setVegsoDatum(Datum* v);
+    static void setAktualisDatum(const Datum& aktualis);
+    static Datum getAktualisDatum();
+    void lejart() const;
+    void hataridoTorol(std::string tipus);
+
+    //copy con
+
 };
 
 #endif // HATARIDO_H
