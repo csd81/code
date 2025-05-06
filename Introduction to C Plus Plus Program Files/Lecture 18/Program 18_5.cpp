1c56317c8f90e116158175c6462dc5f22251c581
daniel cs <csorfolydaniel@gmail.com>
2025-05-06 12:33:14 +0200

    +

--- Introduction to C Plus Plus Program Files/Lecture 18/Program 18_5.cpp
+++ Introduction to C Plus Plus Program Files/Lecture 18/Program 18_5.cpp
@@ -5,33 +5,35 @@
 using namespace std;
 
 class node {
-	bool is_answer; //True if an "answer" node, False if a "question" node
-	string text; //The answer for an answer node, the question otherwise
-	node* false_answer;
+	bool is_answer; 		//True if an "answer" node, False if a "question" node
+
+	string text;		 	//The answer for an answer node, the question otherwise
+	
+	node* false_answer; // we declared this as a pointer to a node object
 	node* true_answer;
 
 public:
-	node() {
-		is_answer = true;
-		text = "rock";
-		false_answer = NULL;
-		true_answer = NULL;
+	node() { 					 // Default constructor
+		is_answer = true;		//This is an answer node
+		text = "rock";			//Default answer is "rock"
+		false_answer = NULL; 	 // false_answer is a pointer to a node object
+		true_answer = NULL;  	// true_answer is a pointer to a node object
 	}
 
-	node(string s) {
-		is_answer = true;
+	node(string s) {								// Constructor with a string argument, which is the answer
+		is_answer = true;  // This is an answer node 
 		text = s;
 		false_answer = NULL;
 		true_answer = NULL;
 	}
 
-	bool ask_question() {
+	bool ask_question() {  					// This function asks the question and gets the answer
 		if (is_answer) {
 			//This is an answer node. 
 			//Guess the answer, and if it's wrong, generate new nodes
 			cout << "OK, I'm ready to guess: is it " << text << "? (Answer Yes or No): ";
 			string answer;
-			cin >> answer;
+			cin >> answer; // Get the answer
 			if ((answer == "Yes") || (answer == "yes") || (answer == "y") || 
 				(answer == "Y")) {
 				return true;
@@ -44,8 +46,8 @@ public:
 				cout << " (The answer to the question should be Yes for " << answer 
 					<< " and No for " << text << ".)" << endl;
 				string question;
-				getline(cin, question);
-				getline(cin, question);
+				getline(cin, question); 
+				getline(cin, question); // Clear the input buffer
 				true_answer = new node(answer);
 				false_answer = new node(text);
 				text = question;
@@ -88,4 +90,40 @@ int main()
 		cout << "Would you like to play again? ";
 		cin >> answer;
 	}                   
+	// the delete operator is not used in this program, which means that the memory allocated for the nodes will not be freed when the program ends.
+	cout << "Thanks for playing!" << endl;
+	
+	
+	return 0;
+	
 }
+
+// This program is a simple implementation of a 20 questions game.
+// The user thinks of an object, and the program tries to guess it by asking questions.
+// If the program guesses wrong, it asks the user to provide a new question and answer.
+// The program uses a binary tree structure to store the questions and answers.
+// The user can play the game multiple times, and the program learns from each game.
+// The program uses a class called "node" to represent each node in the binary tree.
+// The class has methods to ask questions and store new questions and answers.
+// The program uses dynamic memory allocation to create new nodes as needed.
+
+// a binary tree is a tree data structure in which each node has at most two children.
+// The two children are referred to as the left child and the right child.
+// In this program, the left child represents the "No" answer and the right child represents the "Yes" answer.
+// The program uses a recursive approach to traverse the tree and ask questions.
+// The program starts with a default answer of "rock" and builds the tree as the user plays the game.
+
+// the program learns from the user's input and updates the tree structure accordingly.
+// learnig is done by asking the user to provide a new question and answer when the program guesses wrong.
+// The new question is added to the tree, and the new answer is stored in a new node.
+
+// dynamic memory allocation is used to create new nodes as needed.
+// The program uses the "new" operator to allocate memory for new nodes and the "delete" operator to free memory when it is no longer needed.
+
+// the delete operator is not used in this program, which means that the memory allocated for the nodes will not be freed when the program ends.
+// This can lead to memory leaks if the program is run multiple times.
+
+
+// the progam stores the questions and answers in a binary tree structure.
+// The tree is built dynamically as the user plays the game.
+// The program uses a recursive approach to traverse the tree and ask questions.
--- Introduction to C Plus Plus Program Files/Lecture 18/Program 18_6.cpp
+++ Introduction to C Plus Plus Program Files/Lecture 18/Program 18_6.cpp
@@ -28,7 +28,9 @@ public:
 	~node() {
 		if (!is_answer) {
 			delete false_answer;
-			delete true_answer;                            
+			delete true_answer;   
+			cout << "Deleting question: " << text << endl;
+
 		}
 	}
 	                                                             
@@ -96,3 +98,28 @@ int main()
 		cin >> answer;
 	}                   
 }
+
+// This program is a simple implementation of a 20 Questions game using a binary tree structure.
+// The program allows the user to play a guessing game where the computer tries to guess an object the user is thinking of by asking yes/no questions.
+// The user can teach the program new objects and questions by providing feedback.
+// The program uses a binary tree to represent the questions and answers, where each node can be either a question or an answer.
+// The program is designed to be interactive, allowing the user to play multiple rounds of the game.
+// The program uses dynamic memory allocation to create new nodes in the binary tree when the user teaches the program new objects and questions.
+// the program also includes a destructor to free the memory allocated for the nodes when the program ends.
+// this way memory leaks are avoided.
+
+// binary tree is a data structure that consists of nodes, where each node has at most two children.
+// it is implemented using a class called "node".
+// the class has a constructor to initialize the node, a destructor to free memory, and a method to ask questions and process answers.
+// the program uses a recursive approach to traverse the binary tree and ask questions.
+
+// recursion is a programming technique where a function calls itself to solve smaller instances of the same problem.
+// in this program, the ask_question() method is called recursively to traverse the binary tree and ask questions until an answer node is reached.
+
+// the ask_question() method is responsible for asking questions and processing user input.
+// it checks if the current node is an answer or a question, and based on the user's response, it either returns true or false.
+// if the user answers "yes", it continues to the true_answer node, and if the user answers "no", it goes to the false_answer node.
+
+// an answer node represents a specific object that the program can guess.
+// a question node represents a question that helps distinguish between two objects.
+// the program uses a string variable called "text" to store the question or answer for each node.
