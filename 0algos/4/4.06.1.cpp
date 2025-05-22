763bdb0622574438f3cf919d793bc298c4fdb8fd
daniel cs <csorfolydaniel@gmail.com>
2025-05-23 00:49:11 +0200

    +

--- 0algos/4/4.06.1.cpp
+++ 0algos/4/4.06.1.cpp
@@ -1,40 +1,74 @@
-4.6.1.
-#include <stdio.h>
-#include <stdlib.h> struct TElement { struct TElement * next;
-int Data;
+// 4.6.1. Írjon programot, amely számokat olvas be a billentyűzetről addig, amíg a beolvasott szám nem 0! A program fűzze a beolvasott számot egy egyszeresen láncolt lista végére! A -1 gépelése után írja ki a képernyőre a lista tartalmát! Valósítsa meg az add és print függvényeket, valamint a lista felszabadítását! Példa: Type a number: 10 Type a number: 20 Type a number: 30 Type a number: -1 The list: 10 20 30 Type a number: 40 Type a number: 50 Type a number: -1 The list: 10 20 30 40 50 Type a number: 0 4.7. Prímszita
+
+
+// c++11
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int d) : data(d), next(nullptr) {}
 };
-struct TList { struct TElement * Head;
-struct TElement * Actual;
+
+class SinglyLinkedList {
+    Node* head;
+    Node* tail;
+public:
+    SinglyLinkedList() : head(nullptr), tail(nullptr) {}
+    ~SinglyLinkedList() { clear(); }
+
+    // Add a new node with value d at the end
+    void add(int d) {
+        Node* n = new Node(d);
+        if (!head) {
+            head = tail = n;
+        } else {
+            tail->next = n;
+            tail = n;
+        }
+    }
+
+    // Print all elements on one line
+    void print() const {
+        if (!head) {
+            std::cout << "The list is empty\n";
+            return;
+        }
+        std::cout << "The list:";
+        for (Node* p = head; p; p = p->next) {
+            std::cout << " " << p->data;
+        }
+        std::cout << "\n";
+    }
+
+    // Free all nodes
+    void clear() {
+        Node* p = head;
+        while (p) {
+            Node* nxt = p->next;
+            delete p;
+            p = nxt;
+        }
+        head = tail = nullptr;
+    }
 };
-void InitList(struct TList * L) { L->Head = L->Actual = NULL;
-} void Add(struct TList * L, int data) { if (L->Head == NULL) L->Head = L->Actual = (struct TElement *)malloc(sizeof(struct TElement));
-else { L->Actual->next = (struct TElement *)malloc(sizeof(struct TElement));
-L->Actual = L->Actual->next;
-} L->Actual->Data = data;
-L->Actual->next = NULL;
-} void PrintList(struct TList * L) { if (L->Head == NULL) return;
-L->Actual = L->Head;
-printf("The list: ");
-while (L->Actual->next) { printf("%d ", L->Actual->Data);
-L->Actual = L->Actual->next;
-} printf("%d\n", L->Actual->Data);
-} void FreeList(struct TList * L) { struct TElement * tmp;
-if (L->Head == NULL) return;
-L->Actual = L->Head;
-do { tmp = L->Actual;
-L->Actual = L->Actual->next;
-free(tmp);
-tmp = NULL;
-} while (L->Actual);
-} int main() { struct TList List;
-int num;
-InitList(&List);
-do { printf("Type a number: ");
-scanf("%d", &num);
-if (num != 0) { if (num != -1) Add(&List, num);
-else PrintList(&List);
-} } while (num != 0);
-FreeList(&List);
-return 0;
-} 
-4.6.1. Írjon programot, amely számokat olvas be a billentyűzetről addig, amíg a beolvasott szám nem 0! A program fűzze a beolvasott számot egy egyszeresen láncolt lista végére! A -1 gépelése után írja ki a képernyőre a lista tartalmát! Valósítsa meg az add és print függvényeket, valamint a lista felszabadítását! Példa: Type a number: 10 Type a number: 20 Type a number: 30 Type a number: -1 The list: 10 20 30 Type a number: 40 Type a number: 50 Type a number: -1 The list: 10 20 30 40 50 Type a number: 0 4.7. Prímszita
+
+int main() {
+    SinglyLinkedList list;
+    int num;
+
+    while (true) {
+        std::cout << "Type a number: ";
+        std::cin >> num;
+        if (!std::cin) break;       // handle EOF or error
+        if (num == 0) break;        // terminate
+        if (num == -1) {
+            list.print();           // print on -1
+        } else {
+            list.add(num);          // otherwise add
+        }
+    }
+
+    // list destructor will free memory
+    return 0;
+}
--- 0algos/4/4.07.1.cpp
+++ 0algos/4/4.07.1.cpp
@@ -1,58 +1,84 @@
 // c++11
-4.7.1. Kérjünk be egy tetszőleges egész számot (n), majd egy listába tegyük be 2-től n-ig a természetes számokat! Vegyünk ki minden nem prím számot a listából! Ötlet: a lista első eleme (2) prím, minden olyan számot törlünk, amely osztható ezzel a számmal (az első iterációban 2 kivételével az összes páros szám törlődik). A megmaradt elemek közül a következő ismét prím, töröljük ennek is minden többszörösét. Ezt ismételjük addig, amíg a lista végére nem érünk, ekkor írjuk ki a listát! 4.8. Halmazműveletek
+// 4.7.1. Kérjünk be egy tetszőleges egész számot (n), majd egy listába tegyük be 2-től n-ig a természetes számokat! Vegyünk ki minden nem prím számot a listából! Ötlet: a lista első eleme (2) prím, minden olyan számot törlünk, amely osztható ezzel a számmal (az első iterációban 2 kivételével az összes páros szám törlődik). A megmaradt elemek közül a következő ismét prím, töröljük ennek is minden többszörösét. Ezt ismételjük addig, amíg a lista végére nem érünk, ekkor írjuk ki a listát! 4.8. Halmazműveletek
 
  
-#include <stdio.h>
-#include <stdlib.h> struct PrimeArray { int current_number;
-struct PrimeArray* next;
+
+// c++11
+#include <iostream>
+
+struct Node {
+    int value;
+    Node* next;
+    Node(int v) : value(v), next(nullptr) {}
 };
-void UploadPrimeArray(struct PrimeArray*, int);
-void ListPrimeArray(struct PrimeArray);
-void RemoveNotPrimes(struct PrimeArray*);
-void FreePrimeArray(struct PrimeArray*);
-int main() { int MAX_NUMBER;
-struct PrimeArray my_prime_array;
-printf("Pleas enter a number, the orignal list will be generated from 2 to this number.\n");
-scanf("%d", &MAX_NUMBER);
-printf("The original list:\n");
-UploadPrimeArray(&my_prime_array, MAX_NUMBER);
-ListPrimeArray(my_prime_array);
-RemoveNotPrimes(&my_prime_array);
-printf("\n\nThe list, after removing not primes:\n");
-ListPrimeArray(my_prime_array);
-FreePrimeArray(&my_prime_array);
-return 0;
-} void UploadPrimeArray(struct PrimeArray* first, int max_number) { int i;
-struct PrimeArray* actual;
-actual = first;
-actual->current_number = 2;
-actual->next = NULL;
-for (i = 3;
-i <= max_number;
-++i) { actual->next = (struct PrimeArray*)malloc(sizeof(struct PrimeArray));
-actual = actual->next;
-actual->current_number = i;
-actual->next = NULL;
-} } void ListPrimeArray(struct PrimeArray first) { int i = 1;
-struct PrimeArray* actual = &first;
-while (actual) { printf("%3d.element: %3d\n", i++, actual->current_number);
-actual = actual->next;
-} } void RemoveNotPrimes(struct PrimeArray* first) { struct PrimeArray* actual, *prev, *next;
-while (first->next) { actual = first->next;
-prev = first;
-next = actual->next;
-while (actual->next) { if ((actual->current_number % first->current_number) == 0) { free(actual);
-actual = next;
-next = actual->next;
-prev->next = actual;
-} else { prev = actual;
-actual = actual->next;
-next = actual->next;
-} } printf("\nInner steps (%%%d)\n", first->current_number);
-ListPrimeArray(*first);
-first = first->next;
-} } void FreePrimeArray(struct PrimeArray* first) { fel struct PrimeArray* actual = first->next;
-while (actual) { first = actual->next;
-free(actual);
-actual = first;
-} } 
+
+// Build a linked list of integers from 2 up to n
+Node* buildList(int n) {
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    for (int i = 2; i <= n; ++i) {
+        Node* node = new Node(i);
+        if (!head) {
+            head = tail = node;
+        } else {
+            tail->next = node;
+            tail = node;
+        }
+    }
+    return head;
+}
+
+// Remove from the list after p all nodes whose value is a multiple of p->value
+void removeMultiples(Node* p) {
+    Node* prev = p;
+    Node* cur  = p->next;
+    while (cur) {
+        Node* nxt = cur->next;
+        if (cur->value % p->value == 0) {
+            prev->next = nxt;
+            delete cur;
+        } else {
+            prev = cur;
+        }
+        cur = nxt;
+    }
+}
+
+// Sieve of Eratosthenes on the linked list
+void sieve(Node*& head) {
+    for (Node* p = head; p; p = p->next) {
+        removeMultiples(p);
+    }
+}
+
+// Print all values in the list, separated by spaces
+void printList(Node* head) {
+    bool first = true;
+    for (Node* p = head; p; p = p->next) {
+        if (!first) std::cout << ' ';
+        std::cout << p->value;
+        first = false;
+    }
+    std::cout << "\n";
+}
+
+// Free all nodes in the list
+void freeList(Node* head) {
+    while (head) {
+        Node* tmp = head->next;
+        delete head;
+        head = tmp;
+    }
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    if (n < 2) return 0;
+
+    Node* head = buildList(n);
+    sieve(head);
+    printList(head);
+    freeList(head);
+    return 0;
+}
